01cbe0fe9db6abbd8428df2de731e241e3c189e6
Jeff2C <jeff.c.carmichael@gmail.com>
2018-07-17 23:06:59 -0400

    update

--- config/orm.js
+++ config/orm.js
@@ -1,53 +1,55 @@
 var connection = require("./connections.js");
 
-function objToSql(obj){
-	//stores converted object properties
-	var arr = [];
-	//for each property in object
-	for (key in obj){
-		// push sql syntax to arr ex: objectProperty = objectPropertyValue ;
-		arr.push(key + "=" + obj[key]);
-	}
-	//when all properties in object have been pushed to arr return arr as a string
-	return arr.toString();
+function objToSql(obj) {
+    //Converting object to sql.
+    var arr = [];
+    //for each property in object
+    for (key in obj) {
+    //  Format to sql and push to array.
+        arr.push(key + "=" + obj[key]);
+    }
+    //Push into arr return arr as a string.
+    console.log(arr);
+    return arr.toString();
 }
 
 var orm = {
-
-selectAll: function(table, next ){ 
-    var query = "SELECT * FROM " + table + ";"
-connection.query(query, function(err,res ){ 
-    if(err){
-        throw err;
+    //Select all burgers listed in table.
+    selectAll: function (table, next) {
+        var query = "SELECT * FROM " + table + ";"
+        connection.query(query, function (err, res) {
+            if (err) {
+                throw err;
+            }
+            next(res);
+        });
+    },
+    //Add Burger
+    insertOne: function (table, col, val, next) {
+        var query = "INSERT INTO " + table;
+        query += " ( " + col.toString() + ")";
+        query += "VALUES (?) ";
+        connection.query(query, val, function (err, res) {
+            if (err) {
+                throw err;
+            }
+            next(res);
+        });
+    },
+    //Update chosen burger to devoured.
+    updateOne: function (table, obj, condition, next) {
+        //set query
+        var query = "UPDATE " + table;
+        query += " SET " + objToSql(obj);
+        query += " WHERE " + condition + ';';
+        //connect to mysql and perform query
+        connection.query(query, function (err, res) {
+            //error handler
+            if (err) throw err;
+            //callback
+            next(res);
+        })
     }
-    next(res);
-});
-},
-insertOne: function(table,col,val,next){
-    var query = "INSERT INTO "+table;
-    query+= " ( "+col.toString() + ")";
-    query+= "VALUES (?) " ;
-    connection.query(query,val,function(err,res){
-        if(err) {
-            throw err;
-        }
-        next(res);
-    });      
- },
- //update one function *update*
- updateOne: function( table, obj, condition, next){
-    //set query
-    var query = "UPDATE " + table;
-    query += " SET " + objToSql(obj);
-    query += " WHERE " + condition + ';';
-    //connect to mysql and perform query
-    connection.query(query, function(err,res){
-        //error handler
-        if (err) throw err;
-        //callback
-        next(res);
-    })
-}
 
 };
 
--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,28 +2,29 @@ var express = require("express");
 var router = express.Router();
 var burger = require("../models/burger.js");
 
-router.get("/",function(req,res) {
-burger.selectAll(function(burgerdata){
-res.render("index",{burgers: burgerdata});
-});
-});
-
-router.put("/update/:id",function(req,res){
-	//set condition to id of burger
-	var condition = "id = " + req.params.id;
-	//use burger model to set devoured bool to true where condition is true
-	burger.updateOne({"devoured": 1}, condition,
-	//after updating devoured bool redirect to landing page
-	 function(data){
-		res.redirect("/")
+//Diplay all burgers
+router.get("/", function (req, res) {
+	burger.selectAll(function (burgerdata) {
+		res.render("index", { burgers: burgerdata });
 	});
 });
 
-router.post("/create", function(req,res){
-    burger.insertOne("burger_name", req.body.name, function(burgerdata){
-        res.redirect("/");
-    })
-    
+router.put("/update/:id", function (req, res) {
+	//Set devoured status on burger based on id
+	var condition = "id = " + req.params.id;
+	burger.updateOne({ "devoured": 1 }, condition,
+		//after updating devoured status, redirect to landing page
+		function (data) {
+			res.redirect("/")
+		});
+});
+//Create new burger
+router.post("/create", function (req, res) {
+	burger.insertOne("burger_name", req.body.name, function (burgerdata) {
+		//Redirect to landing page
+		res.redirect("/");
+	})
+
 });
 
 module.exports = router;
